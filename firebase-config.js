b8a7eba59bcdd186b03dcf53bf404ee3e34ad334
varunsaxena590-creator <varunsaxena590@gmail.com>
2026-06-13 14:42:21 +0530

    Configure Firebase database sync

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase config goes here. After this is filled, admin panel updates sync for all users.
 // Create a Firebase Realtime Database project and replace the placeholder values below.
 window.VARUN_FIREBASE_CONFIG = {
-  apiKey: "PASTE_API_KEY_HERE",
-  authDomain: "PASTE_AUTH_DOMAIN_HERE",
-  databaseURL: "PASTE_DATABASE_URL_HERE",
-  projectId: "PASTE_PROJECT_ID_HERE",
-  storageBucket: "PASTE_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyBORBCvg3P1fFsAFiUpQgpKpgH-8ffci4",
+  authDomain: "varun-portfolio-4f003.firebaseapp.com",
+  databaseURL: "https://varun-portfolio-4f003-default-rtdb.firebaseio.com",
+  projectId: "varun-portfolio-4f003",
+  storageBucket: "varun-portfolio-4f003.firebasestorage.app",
+  messagingSenderId: "838989504186",
+  appId: "1:838989504186:web:c4be62c7d06219d1672c83"
 };
 
 window.VARUN_FIREBASE_PATH = "portfolio/live";
